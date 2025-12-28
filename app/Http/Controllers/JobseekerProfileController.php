9c938746a89db2e52b58794b17b652f8ad99b5fe
pramisaradiah <diahpramisara@gmail.com>
2025-12-29 02:13:46 +0800

    Menambahkan model, migrations, dan controller

---
+++ app/Http/Controllers/JobseekerProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobseekerProfile;
+use Illuminate\Http\Request;
+
+class JobseekerProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobseekerProfile $jobseekerProfile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobseekerProfile $jobseekerProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JobseekerProfile $jobseekerProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobseekerProfile $jobseekerProfile)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RecruiterProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecruiterProfile;
+use Illuminate\Http\Request;
+
+class RecruiterProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RecruiterProfile $recruiterProfile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RecruiterProfile $recruiterProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RecruiterProfile $recruiterProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RecruiterProfile $recruiterProfile)
+    {
+        //
+    }
+}
